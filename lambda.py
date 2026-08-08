3ed9f8944a496154a885b72047f77fe6b55142dd
Jonah Rosner <rosner1@purdue.edu>
2026-08-08 17:28:28 -0400

    Convert clock to seconds correctly and include NBA

--- lambda.py
+++ lambda.py
@@ -1,42 +1,54 @@
 import os
 import requests
 
+WNBA_URL = 'https://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard'
+NBA_URL = 'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard'
+URLS = [WNBA_URL, NBA_URL]
+
+def clock_to_seconds(clock):
+    minutes, seconds = clock.split(":")
+    return int(minutes) * 60 + int(seconds)
 
 def find_close_games():
     '''Finds close games for given wnba team'''
-    res = requests.get('https://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard')
-    print(res.status_code)
-    res = res.json()
 
-    for event in res["events"]:
-        competition = event["competitions"][0]
+    for url in URLS:
+        res = requests.get(url)
+        print(res.status_code)
+        res = res.json()
 
-        for competitor in competition["competitors"]:
-            team = competitor["team"]
+        for event in res["events"]:
+            competition = event["competitions"][0]
 
-            if team['id'] == os.getenv('WNBA_ID'):
-                print("Hell yea")
-                scores = []
-                for competitor in competition['competitors']:
-                    scores.append(competitor['score'])
-                scoreDif = abs(int(scores[0]) - int(scores[1]))
-                print(scoreDif)
-                status = competition["status"]
+            for competitor in competition["competitors"]:
+                team = competitor["team"]
 
-                clock = status["displayClock"]
-                period = status["period"]
-                status_type = status['type']
+                if team['id'] == os.getenv('WNBA_ID'):
+                    print("Hell yea")
+                    scores = []
+                    for competitor in competition['competitors']:
+                        scores.append(competitor['score'])
+                    scoreDif = abs(int(scores[0]) - int(scores[1]))
+                    print(scoreDif)
+                    status = competition["status"]
 
-                if status_type['name'] != "STATUS_FINAL" and period > 3 and float(clock) <= 300:
-                    print("CLOSE GAME for chosen team")
+                    clock = status["displayClock"]
+                    remaining_seconds = clock_to_seconds(clock)
+                    period = status["period"]
+                    status_type = status['type']
 
-                send_text()
-                save_game(event["id"])
+                    if status_type['name'] != "STATUS_FINAL" and period >= 3 and float(remaining_seconds) <= 300 and scoreDif <= 15:
+                        print("CLOSE GAME for chosen team")
+
+                    send_text()
+                    save_game(event["id"])
 
 def send_text():
     '''Send text message to phone save in env var'''
     pass
 
-def save_game():
+def save_game(id):
     '''Save event id to db to prevent multiple texts'''
     pass
+
+find_close_games()
