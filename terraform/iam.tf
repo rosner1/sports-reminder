328973feec40000d5d5249bd348dc2578185c468
Jonah Rosner <rosner1@purdue.edu>
2026-08-20 12:19:18 -0400

    Give lambda more permissions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -13,3 +13,42 @@ resource "aws_iam_role" "lambda_role" {
     })
 }
 
+resource "aws_iam_role_policy" "lambda_sns_policy" {
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "sns:Publish"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Action = [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem"
+      ]
+
+      Resource = aws_dynamodb_table.notifications.arn
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
